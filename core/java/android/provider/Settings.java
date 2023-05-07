fe3d920f8e10ab700cecbae8d9a56bd4608dae7b
nift4 <nift4@protonmail.com>
2023-05-07 12:52:54 +0200

    Force mouse event as touch [1/2]
    Change-Id: I51ad0e3894e04dc49c572421ca00ba321d3ace15

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6720,6 +6720,11 @@ public final class Settings {
         public static final String QS_HEADER_IMAGE = "qs_header_image";
 
         /**
+         * @hide
+         */
+        public static final String FORCE_MOUSE_AS_TOUCH = "force_mouse_as_touch";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6859,6 +6864,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(AMBIENT_WAKE_GESTURES);
             PRIVATE_SETTINGS.add(TRANSISTENT_TASK_MODE);
             PRIVATE_SETTINGS.add(PREVENT_POINTER_ACCELERATION);
+            PRIVATE_SETTINGS.add(FORCE_MOUSE_AS_TOUCH);
         }
 
         /**
@@ -6874,6 +6880,7 @@ public final class Settings {
             CLONE_TO_MANAGED_PROFILE.add(TEXT_SHOW_PASSWORD);
             CLONE_TO_MANAGED_PROFILE.add(TIME_12_24);
             CLONE_TO_MANAGED_PROFILE.add(TRANSISTENT_TASK_MODE);
+            CLONE_TO_MANAGED_PROFILE.add(FORCE_MOUSE_AS_TOUCH);
         }
 
         /** @hide */
