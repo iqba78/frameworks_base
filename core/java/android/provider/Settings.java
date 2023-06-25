5d922cd4758385dfb40c326b1f878a9be013df91
someone5678 <nemui3353@gmail.com>
2023-06-25 12:00:51 +0900

    base: Guard brightness slider haptic by setting [1/2]
    Change-Id: Ic42c037da8d7cc8378f084ed20205fafc52b95e5
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6689,6 +6689,12 @@ public final class Settings {
         public static final String NAVBAR_STYLE = "navbar_style";
 
         /**
+         * Give haptic feedback on brightness slider changes.
+         * @hide
+         */
+        public static final String BRIGHTNESS_SLIDER_HAPTIC_ENABLED = "brightness_slider_haptic_enabled";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
